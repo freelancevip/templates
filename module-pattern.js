63deeacfe1b88d9284fb387a5537822325b75941
freelancevip <freelancevip@yandex.ru>
2015-09-21 16:51:12 +0300

    Add javascript pattern module

---
+++ module-pattern.js
@@ -0,0 +1,20 @@
+var MyModule = function () {
+	
+	var myPrivateVar = "Private property";
+	
+	var myPrivateMethod = function() {
+        console.log(myPrivateVar);
+    }
+	
+	var module = {};
+
+	module.myPublicProperty = "Public property";
+
+	module. myPublicMethod = function() {
+		console.log(myPrivateVar);
+		console.log(myPrivateMethod());
+	}
+
+	return module;
+	
+}();
